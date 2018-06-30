ae445fee305b6d72befa96edef7b01991b25e819
Daisy Homolka <daisyhomolka@gmail.com>
2018-06-30 20:06:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ return string.toUpperCase()
 function whisper(string) {
 return string.toLowerCase() 
 }
+
+function logShout(string) {
+console.log(string)
+.toLowerCase() 
+}
+
+function sayHiToGrandma(string) {
+if (string lowercase) 
+
+return "I can't hear you!"
+}
